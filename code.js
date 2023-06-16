2433638a3eb5d8eeb55e237af3ec7a5da398f56f
Joao <joaosilva2505@outlook.comgit config --global user.name joaosilva25>
2023-06-15 21:31:51 -0300

    (Clock_Analógico)

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 let colorInput=document.querySelector('.color');
 let body=document.querySelector('body');
-let ponteiro_min=document.querySelector('.point_min');
-let ponteiro_hra=document.querySelector('.ponint_hour');
-let ponteiro_sec=document.querySelector('.point_sec');
+let pointMin=document.querySelector('.point_min');
+let pointHour=document.querySelector('.point_hour');
+let pointSec=document.querySelector('.point_sec');
 
 function changeColor() {
 
@@ -24,8 +24,9 @@ setInterval(() => {
         const minuteRotation = minutes * 6;
         const secondsRotation = sec * 6;
 
-        ponteiro_hra.style.transform=`rotate(${hourRotation}deg)`;
-        ponteiro_min.style.transform=`rotate(${minuteRotation}deg)`;
-        ponteiro_sec.style.transform=`rotate(${secondsRotation}deg`;
+        pointHour.style.transform=`rotate(${hourRotation}deg)`;
+        pointMin.style.transform=`rotate(${minuteRotation}deg)`;
+        pointSec.style.transform=`rotate(${secondsRotation}deg)`;
+
 },1000);
 
